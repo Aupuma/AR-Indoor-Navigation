eee7c4f3fc3010242b913ea700dc49a4de4c6a25
Aupuma <aurepuerta@gmail.com>
2020-03-28 12:35:34 +0100

    Initial commit with basic calibration and map

---
+++ Assets/ARFoundationDemos/ImageTracking/Scripts/NumberManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class NumberManager : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("Number Object in the prefab")]
+    GameObject m_NumberObject;
+
+    public GameObject numberObject
+    {
+        get => m_NumberObject;
+        set => m_NumberObject = value;
+    }
+
+    AimConstraint m_Constraint;
+
+    void OnEnable()
+    {
+        ConstraintSource m_Source = new ConstraintSource();
+        m_Source.sourceTransform = Camera.main.transform;
+        m_Source.weight = 1.0f;
+        m_Constraint = GetComponentInChildren<AimConstraint>();
+        m_Constraint.AddSource(m_Source);
+    }
+
+    public void Enable3DNumber(bool enable)
+    {
+        m_NumberObject.SetActive(enable);
+    }
+}
---
+++ Assets/Scripts/CalibrationManager.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class CalibrationManager : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("Image manager on the AR Session Origin")]
+    ARTrackedImageManager m_ImageManager;
+
+    /// <summary>
+    /// Get the <c>ARTrackedImageManager</c>
+    /// </summary>
+    public ARTrackedImageManager ImageManager
+    {
+        get => m_ImageManager;
+        set => m_ImageManager = value;
+    }
+
+    public Transform targetTransform;
+    public Transform childTransform;
+    public GameObject scene;
+    public GameObject calibrationCanvas;
+    bool isCalibrating = false;
+
+    //void OnEnable()
+    //{
+    //    m_ImageManager.trackedImagesChanged += ImageManagerOnTrackedImagesChanged;
+    //}
+
+    //void OnDisable()
+    //{
+    //    m_ImageManager.trackedImagesChanged -= ImageManagerOnTrackedImagesChanged;
+    //}
+
+
+    //void ImageManagerOnTrackedImagesChanged(ARTrackedImagesChangedEventArgs obj)
+    //{
+    //    if (!isCalibrating) return;
+
+    //    // updated, set prefab position and rotation
+    //    foreach (ARTrackedImage image in obj.added)
+    //    {
+    //        // image is tracking or tracking with limited state, show visuals and update it's position and rotation
+    //        if (image.trackingState == TrackingState.Tracking)
+    //        {
+    //            CalibratePosition(image.transform, childTransform);
+    //            CalibrateRotation(image.transform, childTransform);
+    //        }
+    //    }
+
+    //    foreach (ARTrackedImage image in obj.updated)
+    //    {
+    //        // image is tracking or tracking with limited state, show visuals and update it's position and rotation
+    //        if (image.trackingState == TrackingState.Tracking)
+    //        {
+    //            CalibratePosition(image.transform, childTransform);
+    //            CalibrateRotation(image.transform, childTransform);
+    //        }
+    //    }
+    //}
+
+    //public void ToggleCalibration(bool calibrate)
+    //{
+    //    isCalibrating = calibrate;
+    //}
+
+    public void Calibrate()
+    {
+        CalibrateRotation(targetTransform, childTransform);
+        CalibratePosition(targetTransform, childTransform);
+        scene.SetActive(true);
+        //calibrationCanvas.SetActive(false);
+    }
+
+    public void CalibratePosition(Transform targetTransform, Transform childTransform)
+    {
+        Vector3 differenceVector = targetTransform.position - childTransform.position;
+        childTransform.parent.position = new Vector3(childTransform.parent.position.x + differenceVector.x, childTransform.parent.position.y + differenceVector.y, childTransform.parent.position.z + differenceVector.z);
+    }
+
+    public void CalibrateRotation(Transform targetTransform, Transform childTransform)
+    {
+        //APPLYING CORRECT ROTATION METHODS------------------------------------------------------
+        // Populate the net rotation that you want the child to have.
+        Quaternion desiredRotation = Quaternion.LookRotation(targetTransform.forward, Vector3.up);
+
+        // Create a rotation that undoes the child's rotation, then applies the desired rotation.
+        Quaternion rotationCorrection = desiredRotation * Quaternion.Inverse(childTransform.localRotation);
+
+        // The parent will apply this correction to its child transforms.
+        Vector3 convertedCorrection = Quaternion.ToEulerAngles(rotationCorrection);
+
+        //The object will only change its yaw
+        childTransform.parent.eulerAngles = new Vector3(0, convertedCorrection.y * Mathf.Rad2Deg, 0);
+        //----------------------------------------------------------------------------------------
+    }
+}
